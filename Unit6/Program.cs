bfe099fb4153e9a747030ac80b7afcde09d33f66
Yaroslav <mozgovoj.yaroslav@mail.ru>
2022-10-05 12:36:23 +0300

    006

---
+++ Unit6/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите цеелое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if ( number >= 100 && number <= 999 )
+{
+    int LastDigit = number % 10;
+    Console.WriteLine($"последняя цифра числа => {LastDigit} ");
+}
+else Console.WriteLine("число не трёх значное");
