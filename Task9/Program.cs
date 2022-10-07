4bd430383e90f4fec3e3400c51eeebd338c0d6d1
Yaroslav <mozgovoj.yaroslav@mail.ru>
2022-10-07 20:28:20 +0300

    поиск индекса элемента

---
+++ Task9/Program.cs
@@ -0,0 +1,15 @@
+﻿
+int[] array = { 21, 32, 33, 44, 5, 64, 7, 85, 9, };
+int n = array.Length;
+int find = 64;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index ++;
+}
